15e6da60f9dc708fa1250a86089e0f75f478c311
Vladymir <VladymirMikhaylov@Gmail.com>
2018-08-18 05:00:55 -0500

    lesson 25
    hw - test3
    
    removed validates methods and added different exceptions for "Object already exist" and "not enough space"

--- src/lesson25/GeneralDAO.java
+++ src/lesson25/GeneralDAO.java
@@ -8,30 +8,17 @@ public class GeneralDAO <T> {
         System.out.println("order is: " + order.toString());
     }
 
-    public <V extends IdEntity> void validate(V v)throws Exception{
-        if(v.getId() <= 0){
-            throw new Exception("Id can't be negative");
-        }
-    }
-
-    public <K> void validate(K k){
-        if (k.equals(100)) {
-            System.out.println(true);
-        }else {
-            System.out.println(false);
-        }
-    }
-
     public T save(T t)throws Exception{
-        if(find(t) == null){
-            for(int i = 0; i < DB.length; i++){
-                if(DB[i] == null){
-                    DB[i] = t;
-                    return (T) DB[i];
-                }
+        if(find(t) != null) {
+            throw new Exception("can't save an object. object already exist");
+        }
+        for(int i = 0; i < DB.length; i++){
+            if(DB[i] == null){
+                DB[i] = t;
+                return (T) DB[i];
             }
         }
-        throw new Exception("can't save an object");
+        throw new Exception("can't save an object. Not enough space");
     }
 
     public T[] getAll(){
