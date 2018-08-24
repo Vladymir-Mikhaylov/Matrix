e721f422901ba032987117936b66f90d93139e0f
Abonent63 <AdminGod>
2018-08-24 17:52:26 +0300

    LESSON28
    comparator, comparable
    
    hw - test1

--- src/lesson28/DateComparator.java
+++ src/lesson28/DateComparator.java
@@ -1,4 +1,15 @@
 package lesson28;
 
-public class DateComparator {
+import java.util.Comparator;
+
+public class DateComparator implements Comparator<Capability> {
+    @Override
+    public int compare(Capability o1, Capability o2) {
+        if(o1.getDateCreated().getTime() < o2.getDateCreated().getTime()){
+            return -1;
+        }else if(o1.getDateCreated().getTime() > o2.getDateCreated().getTime()){
+            return 1;
+        }
+        return 0;
+    }
 }
--- src/lesson28/DemoComparator.java
+++ src/lesson28/DemoComparator.java
@@ -1,15 +1,14 @@
 package lesson28;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Date;
 
 public class DemoComparator {
     public static void main(String[] args) {
         Capability capability1 = new Capability(1001, "test", "zzz", true, new Date());
         Capability capability2 = new Capability(1005, "test", "zzz", false, new Date());
-        Capability capability3 = new Capability(900, "test", "zzz", true, new Date());
-        Capability capability4 = new Capability(900, "test", "zzz", false, new Date());
+        Capability capability3 = new Capability(600, "test", "zzz", true, new Date());
+        Capability capability4 = new Capability(700, "test", "zzz", false, new Date());
 
         ArrayList<Capability> capabilities = new ArrayList<>();
         capabilities.add(capability1);
@@ -17,18 +16,10 @@ public class DemoComparator {
         capabilities.add(capability3);
         capabilities.add(capability4);
 
-        capabilities.sort(new IsActiveComparator());
+        capabilities.sort(new DateComparator());
 
         System.out.println(capabilities);
 
-        Comparator<Capability> comparator = new Comparator<Capability>() {
-            @Override
-            public int compare(Capability o1, Capability o2) {
-                if(!o1.getChannelName().equals(o2.getChannelName())){
-                    return o1.getChannelName().compareTo(o2.getChannelName());
-                }
-                return 0;
-            }
-        };
+
     }
 }
