cb47a0b78664e7d44428017225a1e4119162e1e3
Abonent63 <AdminGod>
2018-09-13 16:16:36 +0300

    LESSON30
    hw
    
    left methods:
    all methods done.
    
    left testing of them

--- src/lesson30/task2/EmployeeDAO.java
+++ src/lesson30/task2/EmployeeDAO.java
@@ -60,14 +60,15 @@ public class EmployeeDAO {
 
     public ArrayList<Employee> employeesByTeamLead(Employee lead){
         ArrayList<Employee> result = new ArrayList<>();
-        //todo
-        /**
-        for(Employee e : employees){
-            if(e != null && e.getPosition().equals(lead.getPosition())){
-                result.add(e);
+        if(lead.getProjects() != null){
+            for(Project project : lead.getProjects()){
+                for(Employee empl : employeesByProject(project.getName())){
+                    if(empl.getPosition() != Position.TEAM_LEAD){
+                        result.add(empl);
+                    }
+                }
             }
         }
-         */
         return result;
     }
 
@@ -101,9 +102,16 @@ public class EmployeeDAO {
         ArrayList<Employee> result = new ArrayList<>();
 
         if(employee.getProjects() != null){
-
+            for(Project project : employee.getProjects()){
+                for(Employee empl : employeesByProject(project.getName())){
+                    if(empl.getPosition() == Position.TEAM_LEAD){
+                        result.add(empl);
+                    }
+                }
+            }
         }
 
         return result;
     }
+
 }
