76a7990edbf8b934ddcd5806075e8b651741e918
Abonent63 <AdminGod>
2018-09-24 18:55:57 +0300

    LESSON33
    hw2 - edits

--- src/lesson33/hw2/Demo.java
+++ src/lesson33/hw2/Demo.java
@@ -5,45 +5,63 @@ import org.apache.commons.io.IOUtils;
 import java.io.*;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //String path = new String("E:\\MEGA\\PT\\java-core-grom_fixed\\test3.txt");
         readFileByConsolePath();
     }
 
-    public static void readFileByConsolePath(){
-        BufferedReader inputStreamReader = new BufferedReader( new InputStreamReader(System.in));
-        FileWriter fileWriter = null;
-        PrintWriter writer = null;
+    public static void readFileByConsolePath()throws Exception{
+        String path = readPath();
+        validate(path);
+        readFileAndWrite(path);
+    }
 
-        System.out.println("Please, enter file path for read: ");
+    private static void validate(String path) throws Exception {
+        validateIfExist(path);
+        validateIfCanRead(path);
+    }
+
+    private static void validateIfExist(String path) throws FileNotFoundException {
+        File file = new File(path);
+        if(!file.exists()){
+            System.out.println("File with path " + path + " not found");
+            throw new FileNotFoundException();
+        }
+    }
+
+    private static void validateIfCanRead(String path) throws Exception {
+        File file = new File(path);
+        if(!file.canRead()){
+            System.out.println("File with path " + path + " can't be read");
+            throw new Exception();
+        }
+    }
+
+    private static String readPath(){
+        BufferedReader inputStreamReader = new BufferedReader( new InputStreamReader(System.in));
+        String path = "";
         try {
-            String path = inputStreamReader.readLine();
-            //check if file exists
-            FileReader reader;
-            try {
-                reader = new FileReader(path);
-            }catch (FileNotFoundException ex){
-                System.out.println("File with path " + path + " not found");
-                return;
-            }
-            //print data from file by next @path@
-            BufferedReader br = new BufferedReader(reader);
-            try {
-                String line;
-                while ( (line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } catch (IOException e) {
-                System.err.println("Can't read file by path " + path);
-            }
-            finally {
-                IOUtils.closeQuietly(reader);
-                IOUtils.closeQuietly(br);
-            }
+            path = inputStreamReader.readLine();
         } catch (IOException e) {
             System.err.println("Reading from keyboard failed");
         } finally {
             IOUtils.closeQuietly(inputStreamReader);
+        }
+        return path;
+    }
+
+    private static void readFileAndWrite(String path) throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            String line;
+            while ( (line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Can't read file by path " + path);
+        }
+        finally {
+            IOUtils.closeQuietly(br);
         }
     }
 }
