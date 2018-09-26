f6939e1e30ce4252c374312d02baa344970ba47c
Abonent63 <AdminGod>
2018-09-26 17:44:45 +0300

    LESSON33
    refactoring 1

--- src/lesson33/hw1/Demo.java
+++ src/lesson33/hw1/Demo.java
@@ -1,68 +1,13 @@
 package lesson33.hw1;
 
-import org.apache.commons.io.IOUtils;
-
 import java.io.*;
 
 public class Demo {
 
     public static void main(String[] args) throws FileNotFoundException {
         String path = "E:\\MEGA\\PT\\java-core-grom_fixed\\test3.txt";
-        writeToFileFromConsole(path);
-    }
-
-    public static void writeToFileFromConsole(String path) throws FileNotFoundException {
-        //check if file exists
-        validate(path);
-
-        System.out.println("Enter file content to write: ");
-
-        //write printed data to file @path@
-        write(read(), path);
+        Lesson33Task1.writeToFileFromConsole(path);
     }
 
 
-    private static String read(){
-        BufferedReader inputStreamReader = new BufferedReader( new InputStreamReader(System.in));
-        String consoleData = null;
-        try {
-            String enteredText = inputStreamReader.readLine();
-
-            //collect printed data
-            while(!enteredText.equals("wr")) {
-                consoleData += "\r\n" + enteredText;
-                enteredText = inputStreamReader.readLine();
-            }
-
-        } catch (IOException e) {
-            System.err.println("Reading from keyboard failed");
-        } finally {
-            IOUtils.closeQuietly(inputStreamReader);
-        }
-        return consoleData;
-    }
-
-    private static void write(String consoleData, String path){
-        FileWriter fileWriter = null;
-        PrintWriter writer = null;
-        try {
-            fileWriter = new FileWriter(path, true);
-            writer = new PrintWriter(fileWriter);
-            writer.append("\r\n" + consoleData);
-        } catch (IOException e) {
-            System.err.println("We can't write to file!");
-        } finally {
-            IOUtils.closeQuietly(fileWriter);
-            IOUtils.closeQuietly(writer);
-        }
-    }
-
-
-    private static void validate(String path) throws FileNotFoundException {
-        File file = new File(path);
-        if(!file.exists()){
-            System.out.println("File with path " + path + " not found");
-            throw new FileNotFoundException();
-        }
-    }
 }
