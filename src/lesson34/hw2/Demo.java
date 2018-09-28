4a5d8e7e72217458e8c1eada73daf1ca7d14b099
Abonent63 <AdminGod>
2018-09-28 20:21:31 +0300

    LESSON34 task3 add demo class

--- src/lesson34/hw2/Demo.java
+++ src/lesson34/hw2/Demo.java
@@ -1,7 +1,5 @@
 package lesson34.hw2;
 
-import lesson34.Solution;
-
 public class Demo {
     public static void main(String[] args) throws Exception{
         String pathFrom = new String("E:\\MEGA\\PT\\java-core-grom_fixed\\test3.txt");
--- src/lesson34/hw3/Demo.java
+++ src/lesson34/hw3/Demo.java
@@ -1,4 +1,13 @@
 package lesson34.hw3;
 
 public class Demo {
+    public static void main(String[] args) throws Exception{
+        String pathFrom = new String("E:\\MEGA\\PT\\java-core-grom_fixed\\test3.txt");
+        String pathTo = new String("E:\\MEGA\\PT\\java-core-grom_fixed\\test1.txt");
+
+        //Solution.copyFileContent(pathFrom, pathTo);
+        Solution.copyFileCOntentByApacheIO(pathFrom, pathTo);
+    }
 }
+
+
